6eb2b839327c81d07dbfa47039d15fc65caa302b
Naoki Ohta <n.ohta@meleap.com>
2020-05-21 14:46:34 +0900

    [ch04] Show Loss Graph.

--- ch04/train_neuralnet.py
+++ ch04/train_neuralnet.py
@@ -26,18 +26,18 @@ for i in range(iters_num):
     batch_mask = np.random.choice(train_size, batch_size)
     x_batch = x_train[batch_mask]
     t_batch = t_train[batch_mask]
-    
+
     # 勾配の計算
     #grad = network.numerical_gradient(x_batch, t_batch)
     grad = network.gradient(x_batch, t_batch)
-    
+
     # パラメータの更新
     for key in ('W1', 'b1', 'W2', 'b2'):
         network.params[key] -= learning_rate * grad[key]
-    
+
     loss = network.loss(x_batch, t_batch)
     train_loss_list.append(loss)
-    
+
     if i % iter_per_epoch == 0:
         train_acc = network.accuracy(x_train, t_train)
         test_acc = network.accuracy(x_test, t_test)
@@ -46,12 +46,23 @@ for i in range(iters_num):
         print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
 
 # グラフの描画
-markers = {'train': 'o', 'test': 's'}
-x = np.arange(len(train_acc_list))
-plt.plot(x, train_acc_list, label='train acc')
-plt.plot(x, test_acc_list, label='test acc', linestyle='--')
-plt.xlabel("epochs")
-plt.ylabel("accuracy")
-plt.ylim(0, 1.0)
+# Accuracy
+# markers = {'train': 'o', 'test': 's'}
+# x = np.arange(len(train_acc_list))
+# plt.plot(x, train_acc_list, label='train acc')
+# plt.plot(x, test_acc_list, label='test acc', linestyle='--')
+# plt.xlabel("epochs")
+# plt.ylabel("accuracy")
+# plt.ylim(0, 1.0)
+# plt.legend(loc='lower right')
+# plt.show()
+
+# Loss
+markers = {'loss': 'o'}
+x = np.arange(len(train_loss_list))
+plt.plot(x, train_loss_list, label='train loss')
+plt.xlabel("iteration")
+plt.ylabel("loss")
+plt.ylim(0, 3.0)
 plt.legend(loc='lower right')
 plt.show()
